0963f8e2e681ac7dcf218e6e0b8a7308e6889dd7
Lucas <lucas.vanstaden@enjo.com>
2020-12-22 04:33:46 +0800

    Initial release

---
+++ Plugin/AddDataToOrdersGrid.php
@@ -0,0 +1,61 @@
+<?php
+namespace Ittools\OrderGridComments\Plugin;
+
+/**
+ * Class AddDataToOrdersGrid
+ */
+class AddDataToOrdersGrid
+{
+    /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * AddDataToOrdersGrid constructor.
+     *
+     * @param \Psr\Log\LoggerInterface $customLogger
+     * @param array $data
+     */
+    public function __construct(
+        \Psr\Log\LoggerInterface $customLogger,
+        array $data = []
+    ) {
+        $this->logger   = $customLogger;
+    }
+
+    /**
+     * @param \Magento\Framework\View\Element\UiComponent\DataProvider\CollectionFactory $subject
+     * @param \Magento\Sales\Model\ResourceModel\Order\Grid\Collection $collection
+     * @param $requestName
+     * @return mixed
+     */
+    public function afterGetReport($subject, $collection, $requestName)
+    {
+        if ($requestName !== 'sales_order_grid_data_source') {
+            return $collection;
+        }
+
+        if ($collection->getMainTable() === $collection->getResource()->getTable('sales_order_grid')) {
+            try {
+                $orderCommentsTableName = $collection->getResource()->getTable('sales_order_status_history');
+                $subquery = new \Zend_Db_Expr("(select parent_id,
+    group_concat(comment order by parent_id asc separator ' <br/> ') as comment
+  from
+    {$orderCommentsTableName}
+  group by
+    parent_id)");
+                $collection->getSelect()->joinLeft(
+                    ['sosh' => $subquery ],
+                    'sosh.parent_id = main_table.entity_id',
+                    ['sosh.comment']
+                );
+            } catch (\Zend_Db_Select_Exception $selectException) {
+                // Do nothing in that case
+                $this->logger->log(100, $selectException);
+            }
+        }
+        $this->logger->log(100, $collection->getSelectSql(true));
+        return $collection;
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+## What doe sit do:
+
+Place a filterable column in admin sales order grid that displays all order comments, and is also filterable
+
